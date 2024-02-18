545b03049ac3734fa05b529fce707d6bd8625296
Bali Zsolt <bali.zsolt05@gmail.com>
2024-02-18 14:23:47 +0100

    Added AuthController (closes #9)

---
+++ NoteShare.API/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NoteShare.Core.Services;
+using NoteShare.Data.Entities;
+using NoteShare.Models.Auth;
+
+namespace NoteShare.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        // POST: api/Auth/register
+        [HttpPost]
+        [Route("register")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult> FoodWasteReducerUserRegister([FromBody] RegisterDto registerDto)
+        {
+            await _authService.Register(registerDto);
+            return Ok();
+        }
+
+        // POST: api/Auth/login
+        [HttpPost]
+        [Route("login")]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<ActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var authResponse = await _authService.Login(loginDto);
+
+            if (authResponse == null)
+            {
+                return Unauthorized();
+            }
+            return Ok(authResponse);
+        }
+    }
+}
